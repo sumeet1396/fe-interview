d8f97d0e448933057d8460e9ea18a9da483955e7
sumeet <sumeet1396@gmail.com>
2024-09-24 23:30:03 +0530

    debounce code added

---
+++ interview-questions/01-debounce.js
@@ -0,0 +1,25 @@
+/**
+ * Creates a debounced function that delays invoking `func` until after `wait` milliseconds
+ * have elapsed since the last time the debounced function was invoked.
+ *
+ * @param {Function} func - The function to debounce.
+ * @param {number} wait - The number of milliseconds to delay.
+ * @returns {Function} - Returns the new debounced function.
+ */
+
+const debounce = (func, wait) => {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(context, args), wait);
+    };
+}
+
+let startTime = Date.now()
+
+const debounceFn = debounce(() => {
+    console.log(`Fetch Data called after ${Date.now() - startTime}ms`);
+  }, 500);
+  
+console.log(debounceFn())
