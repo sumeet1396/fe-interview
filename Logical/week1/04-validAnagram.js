cb732cc04cd5a4f49cd39b0ff9e56f024ac05ced
sumeet <sumeet1396@gmail.com>
2026-03-04 01:07:43 +0530

    folder structure update

--- Logical/week1/04-validAnagram.js
+++ Logical/week1/04-validAnagram.js
@@ -9,7 +9,7 @@ var isAnagram = function(s, t) {
     for (let i =0; i <s.length; i++) {
         if (t.includes(s[i])) {
             str += s[i]
-            t.replace(s[i], '')
+            t = t.replace(s[i], '')
         }
     }
 
@@ -18,3 +18,23 @@ var isAnagram = function(s, t) {
 
 console.log(isAnagram("anagram","nagaram"))
 console.log(isAnagram("rat","car"))
+console.log(isAnagram("aacc","ccac"))
+
+// optimized
+var isAnagram2 = function(s, t) {
+  if (t.length !== s.length) return false
+  
+  let obj1 = {}
+  let obj2 = {}
+  for (let i = 0; i < s.length; i++) {
+    obj1[s[i]] = (obj1[s[i]] || 0) + 1
+    obj2[t[i]] = (obj2[t[i]] || 0) + 1
+  }
+
+  for (const key in obj1) if (obj1[key] !== obj2[key]) return false
+  return true;
+};
+
+console.log(isAnagram2("anagram","nagaram"))
+console.log(isAnagram2("rat","car"))
+console.log(isAnagram2("aacc","ccac"))
