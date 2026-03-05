9fea8bc78f1b59352a6c54e382ec8f5e65342ae4
sumeet <sumeet1396@gmail.com>
2026-03-06 00:06:39 +0530

    slice & splice method updated for array data structure

--- Data-Structure/array/array.js
+++ Data-Structure/array/array.js
@@ -42,6 +42,65 @@ class MyArray {
       delete this.data[this.length - 1];
       this.length--;
     }
+
+    slice(start = 0, end = this.length) {
+      const newArray = new MyArray();
+
+      if (start < 0) start = this.length + start;
+      if (end < 0) end = this.length + end;
+
+      start = Math.max(0, start);
+      end = Math.min(this.length, end);
+
+      for (let i = start; i < end; i++) {
+        newArray.data[newArray.length] = this.data[i];
+        newArray.length++;
+      }
+
+      return newArray;
+    }
+    
+    splice(start, deleteCount = 0, ...items) {
+      const removed = [];
+
+      if (start < 0) start = this.length + start;
+      if (start < 0) start = 0;
+      if (start > this.length) start = this.length;
+
+      deleteCount = Math.min(deleteCount, this.length - start);
+
+      for (let i = 0; i < deleteCount; i++) {
+        removed.push(this.data[start + i]);
+      }
+
+      const shift = items.length - deleteCount;
+
+      if (shift > 0) {
+        for (let i = this.length - 1; i >= start + deleteCount; i--) {
+          this.data[i + shift] = this.data[i];
+        }
+      }
+
+      if (shift < 0) {
+        for (let i = start + deleteCount; i < this.length; i++) {
+          this.data[i + shift] = this.data[i];
+        }
+      }
+
+      for (let i = 0; i < items.length; i++) {
+        this.data[start + i] = items[i];
+      }
+
+      this.length += shift;
+
+      return removed;
+    }
+
+    print() {
+        return Object.values(this.data)
+    }
+
+   
   }
   
   const myArray = new MyArray();
@@ -59,4 +118,18 @@ class MyArray {
   console.log(myArray);
   myArray.shift();
   console.log(myArray);
+  console.log(myArray.slice(1, 2));
+  myArray.push(1)
+  myArray.push(2)
+  myArray.push(3)
+  myArray.push(4)
+  console.log(myArray.print())
+  myArray.splice(2,2)
+  console.log(myArray.print())
+  myArray.splice(2)
+  console.log(myArray.print())
+  myArray.splice(2,0)
+  console.log(myArray.print())
+  myArray.splice(1,0, "temp")
+  console.log(myArray.print())
   
